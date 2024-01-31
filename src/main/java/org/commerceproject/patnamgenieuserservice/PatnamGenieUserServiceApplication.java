b136826350e369173a99573a442be28a971b26c5
msdpramod <kuppapramod@gmail.com>
2024-01-31 22:30:31 +0530

    hi

---
+++ src/main/java/org/commerceproject/patnamgenieuserservice/PatnamGenieUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.commerceproject.patnamgenieuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatnamGenieUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatnamGenieUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/commerceproject/patnamgenieuserservice/PatnamGenieUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commerceproject.patnamgenieuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatnamGenieUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
